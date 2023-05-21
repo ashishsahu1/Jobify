3bf2b00992ad59184025166abfb3935332bcb297
Ashish Sahu <ashishsahu10428@gmail.com>
2023-05-22 02:12:48 +0530

    Header icon and Profile photo added

--- app/index.js
+++ app/index.js
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, ScrollView, SafeAreaView, Text, Image } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome></Welcome>
+          <Popularjobs></Popularjobs>
+          <Nearbyjobs></Nearbyjobs>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
